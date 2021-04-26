b2061894909f5770a0dbb58d9fad61af1ccfb21e
Qu4k3r <neves0007@gmail.com>
2021-04-26 16:19:15 -0300

    Commit inicial | Pair programming inicial, vamos com tudo :rocket

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Provider from './context/Provider';
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <Provider>
+      <Router>
+        <Switch>
+          {/* <Route path="/perfil" component={ Profile } /> */}
+          <Route exact path="/" component={ Login } />
+        </Switch>
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
