87ebd51a6d4ee920e587f87b46b0abd337501092
giovani.diccini <giovani-diccini@pop-os.localdomain>
2021-04-27 16:18:22 -0300

    implements profile page features

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,45 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import '../styles/Profile.css';
 
 export default function Profile() {
   const searchIcon = false;
+
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  const clearLocalStorage = () => {
+    localStorage.removeItem('email');
+    localStorage.removeItem('mealsToken');
+    localStorage.removeItem('cocktailsToken');
+    localStorage.removeItem('doneRecipes');
+    localStorage.removeItem('favoriteRecipes');
+    localStorage.removeItem('inProgressRecipes');
+  };
+
   return (
-    <div>
+    <div className="Profile">
       <Header title="Perfil" searchIcon={ searchIcon } />
-      <h6>comidas</h6>
+      <main>
+        <p data-testid="profile-email">{user.email}</p>
+        <Link to="/receitas-feitas">
+          <button type="button" data-testid="profile-done-btn">Receitas Feitas</button>
+        </Link>
+        <Link to="/receitas-favoritas">
+          <button type="button" data-testid="profile-favorite-btn">
+            Receitas Favoritas
+          </button>
+        </Link>
+        <Link to="/">
+          <button
+            type="button"
+            data-testid="profile-logout-btn"
+            onClick={ () => clearLocalStorage() }
+          >
+            Sair
+          </button>
+        </Link>
+      </main>
     </div>
   );
 }
