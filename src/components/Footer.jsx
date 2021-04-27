e72dcb79ca585617206a00870cb1c863b7fba313
Kevin Fraga <kevinciolac@gmail.com>
2021-04-27 14:54:50 -0300

    footer styles fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/footer.css';
+import '../styles/Footer.css';
 
 function Footer() {
   return (
--- src/pages/Foods.jsx
+++ src/pages/Foods.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default function Foods() {
   const searchIcon = true;
@@ -7,6 +8,7 @@ export default function Foods() {
     <div>
       <Header title="Comidas" searchIcon={ searchIcon } />
       <h6>comidas</h6>
+      <Footer />
     </div>
   );
 }
