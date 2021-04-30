08167c97ea7781ce0bc6c0532b7209abd7477810
Kevin Fraga <kevinciolac@gmail.com>
2021-04-30 17:16:12 -0300

    hooks personalizados

---
+++ src/components/IngredientCard.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { arrayOf } from 'prop-types';
+import { fetchIngredientImage } from '../services/APIEndpoints';
+import '../styles/ExploreIngredients.css';
+
+export default function IngredientCard({ item, index }) {
+  let isMeal = false;
+  if (item.strIngredient) isMeal = true;
+  const ingredientImage = fetchIngredientImage(item, isMeal);
+  return (
+    <div
+      key={ isMeal ? item.strIngredient : item.strIngredient1 }
+      data-testid={ `${index}-ingredient-card` }
+      className="ingredient-card"
+    >
+      <img
+        src={ ingredientImage }
+        alt={ isMeal ? item.strIngredient : item.strIngredient1 }
+        data-testid={ `${index}-card-img` }
+      />
+      <p data-testid={ `${index}-card-name` }>
+        {isMeal ? item.strIngredient : item.strIngredient1}
+      </p>
+    </div>
+  );
+}
+
+IngredientCard.propTypes = {
+  item: arrayOf({}),
+}.isRequired;
--- src/services/APIEndpoints.js
+++ src/services/APIEndpoints.js
@@ -33,3 +33,22 @@ export async function fetchDrinkByCategory(category) {
   const response = await getDrinks.json();
   return response;
 }
+
+export async function fetchMealsIngredientList() {
+  const ingredientListAPI = 'https://www.themealdb.com/api/json/v1/1/list.php?i=list';
+  const ingredientList = await fetch(ingredientListAPI);
+  const response = await ingredientList.json();
+  return response.meals;
+}
+
+export async function fetchDrinksIngredientList() {
+  const ingredientListAPI = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list';
+  const ingredientList = await fetch(ingredientListAPI);
+  const response = await ingredientList.json();
+  return response.drinks;
+}
+
+export function fetchIngredientImage(item, isMeal) {
+  const parameter = isMeal ? item.strIngredient : item.strIngredient1;
+  return `https://www.themealdb.com/images/ingredients/${parameter}-Small.png`;
+}
